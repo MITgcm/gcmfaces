9b666df182b130c0cdd119855ed80b8989256d60
gaelforget <gforget@mit.edu>
2017-03-15 21:44:36 -0400

    add another version as comment

--- gcmfaces_misc/figureL.m
+++ gcmfaces_misc/figureL.m
@@ -1,4 +1,5 @@
 
-%figure; set(gcf,'Units','Normalized','Position',[0.1 0.2 0.8 0.8]);
+% figure; set(gcf,'Units','Normalized','Position',[0.1 0.2 0.8 0.8]);
 % figure; set(gcf,'Units','Normalized','Position',[0. 0.2 0.4 0.8]);
+% figure; set(gcf,'Units','Normalized','Position',[-1.7 0.2 0.8 0.8]);
 figure;
