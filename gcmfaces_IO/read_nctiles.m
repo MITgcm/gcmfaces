f6ca02a2bc8ea78a1130e1301bd97735897d796e
gaelforget <gforget@mit.edu>
2018-01-26 15:59:39 -0500

    use 'netcdf_x' octave forge implementation in place of 'netcdf.x' (native Matlab implementation)

--- gcmfaces_IO/read_nctiles.m
+++ gcmfaces_IO/read_nctiles.m
@@ -44,20 +44,20 @@ if ~test1;
   test2=(length(tmp1)~=length(nctiles.no));
 end;
 
-if test1|test2;
+if test1||test2;
   %build map of tile indices
   if (length(dir([fileName '*']))==mygrid.nFaces);
     nctiles.map=NaN*mygrid.XC; 
     for ff=1:mygrid.nFaces; nctiles.map{ff}(:)=ff; end;
   else;
     fileIn=sprintf('%s.%04d.nc',fileName,1);
-    nc=netcdf.open(fileIn,0);
-    vv = netcdf.inqVarID(nc,fldName);
-    [varname,xtype,dimids,natts]=netcdf.inqVar(nc,vv);
+    nc=netcdf_open(fileIn,0);
+    vv = netcdf_inqVarID(nc,fldName);
+    [varname,xtype,dimids,natts]=netcdf_inqVar(nc,vv);
     tileSize=zeros(1,2);
-    [tmp,tileSize(1)] = netcdf.inqDim(nc,dimids(1));
-    [tmp,tileSize(2)] = netcdf.inqDim(nc,dimids(2));
-    netcdf.close(nc);
+    [tmp,tileSize(1)] = netcdf_inqDim(nc,dimids(1));
+    [tmp,tileSize(2)] = netcdf_inqDim(nc,dimids(2));
+    netcdf_close(nc);
     nctiles.map=gcmfaces_loc_tile(tileSize(1),tileSize(2));
   end;
   %determine tile list
@@ -82,13 +82,13 @@ for ff=1:length(nctiles.no);
 
 %read one tile
 fileIn=sprintf('%s.%04d.nc',fileName,ff);
-nc=netcdf.open(fileIn,0);
+nc=netcdf_open(fileIn,0);
 
-vv = netcdf.inqVarID(nc,fldName);
-[varname,xtype,dimids,natts]=netcdf.inqVar(nc,vv);
+vv = netcdf_inqVarID(nc,fldName);
+[varname,xtype,dimids,natts]=netcdf_inqVar(nc,vv);
 
-[dimname,siz(1)] = netcdf.inqDim(nc,dimids(1));
-[dimname,siz(2)] = netcdf.inqDim(nc,dimids(2));
+[dimname,siz(1)] = netcdf_inqDim(nc,dimids(1));
+[dimname,siz(2)] = netcdf_inqDim(nc,dimids(2));
 siz=[siz length(mygrid.RC)];
 
 if ~isempty(tt);
@@ -105,7 +105,7 @@ if ~isempty(tt);
     count=[siz(1) siz(2) length(kk) nt];
   end;
 else
-  [dimname,nt] = netcdf.inqDim(nc,dimids(end));
+  [dimname,nt] = netcdf_inqDim(nc,dimids(end));
   if length(dimids)==2;
     start=[0 0];
     count=[siz(1) siz(2)];
@@ -120,9 +120,9 @@ else
     count=[siz(1) siz(2) length(kk) nt];
   end;
 end;
-fldTile=netcdf.getVar(nc,vv,start,count);
+fldTile=netcdf_getVar(nc,vv,start,count);
 fldTile=squeeze(fldTile);
-netcdf.close(nc);
+netcdf_close(nc);
 
 %initialize fld (full gcmfaces object)
 if ff==1;
