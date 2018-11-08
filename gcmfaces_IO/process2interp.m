4c9b06c6c500847740964dc9426bedcdabd7fefe
Lauren Milechin <lauren.milechin@mit.edu>
2018-11-08 14:21:51 -0500

    changes for processing sample3 (cs510) data

--- gcmfaces_IO/process2interp.m
+++ gcmfaces_IO/process2interp.m
@@ -1,4 +1,4 @@
-function [listInterp,listNot]=process2interp(dirDiags,fileDiags,varargin);
+function [listInterp,listNot]=process2interp(dirDiags,fileDiags,subDir,varargin);
 % [listInterp,listNot]=PROCESS2INTERP(dirDiags,fileDiags);
 % []=PROCESS2INTERP(dirDiags,fileDiags,listInterp);
 %
@@ -18,89 +18,113 @@ filReadme=[dirDiags filesep 'README'];
 %% ======== PART 1 =======
 
 %search for fileDiags in subdirectories
-[subDir]=rdmds_search_subdirs(dirDiags,fileDiags);
+%[subDir]=rdmds_search_subdirs(dirDiags,fileDiags);
 %read meta file to get list of variables
 [meta]=rdmds_meta([dirDiags subDir fileDiags]);
 %set listInterp based on available_diagnostics.log
 listInterp={};
 listNot={};
 for ii=1:length(meta.fldList);
-  %get units and long name from available_diagnostics.log
-  [avail_diag]=read_avail_diag(filAvailDiag,meta.fldList{ii});
-  if ~isempty(avail_diag);
-  if strcmp(avail_diag.loc_h,'C');
-    ndiags=length(listInterp)+1;
-    listInterp={listInterp{:},deblank(meta.fldList{ii})};
-  else;
-    listNot={listNot{:},deblank(meta.fldList{ii})};
-  end;
-  end;
+    %get units and long name from available_diagnostics.log
+    [avail_diag]=read_avail_diag(filAvailDiag,meta.fldList{ii});
+    if ~isempty(avail_diag);
+        if strcmp(avail_diag.loc_h,'C');
+            ndiags=length(listInterp)+1;
+            listInterp={listInterp{:},deblank(meta.fldList{ii})};
+        else;
+            listNot={listNot{:},deblank(meta.fldList{ii})};
+        end;
+    end;
 end;
 
-if nargin==2; return; end;
+if nargin==3; return; end;
 
 %% ======== PART 2 =======
 
-if nargin==3; listInterp=varargin{1}; end;
+if nargin>=4; listInterp=varargin{1}; end;
 if ischar(listInterp); listInterp={listInterp}; end;
 
-if isempty(dir('interp_precomputed.mat'));
-  lon=[-179.75:0.5:179.75]; lat=[-89.75:0.5:89.75];
-  [lat,lon] = meshgrid(lat,lon);
-  interp=gcmfaces_interp_coeffs(lon(:),lat(:));
-  %save interp_precomputed.mat lon lat interp;
+if isempty(dir(fullfile(dirDiags,'interp_precomputed.mat')));
+    lon=[-179.75:0.5:179.75]; lat=[-89.75:0.5:89.75];
+    [lat,lon] = meshgrid(lat,lon);
+    interp=gcmfaces_interp_coeffs(lon(:),lat(:));
+    %save interp_precomputed.mat lon lat interp;
 else;
-  fprintf('reloading interp_precomputed.mat...\n');
-  load('interp_precomputed.mat');
+    fprintf('reloading interp_precomputed.mat...\n');
+    load(fullfile(dirDiags,'interp_precomputed.mat'));
 end;
 
 if ~isdir(dirOut); mkdir(dirOut); end;
 
 for ii=1:length(listInterp);
-
-tic;
-
-nameDiag=deblank(listInterp{ii});
-fprintf(['processing ' nameDiag '... \n']);
-
-if ~isempty(dir([dirOut nameDiag filesep nameDiag '*.data'])); 
-  fprintf(['\n Files were found: ' dirOut nameDiag filesep '*.data']); 
-  test0=fprintf('\n Do you want to continue despite risk of overwriting them?');
-  test0=input('\n >> If yes then please type 1 (otherwise just hit return).\n');
-  if isempty(test0)||test0~=1; fprintf(['... skipping ' nameDiag '\n\n']); continue; end;
-end;
-
-jj=find(strcmp(deblank(meta.fldList),nameDiag));
-myDiag=rdmds2gcmfaces([dirDiags subDir fileDiags '*'],NaN,'rec',jj);
-listFiles=dir([dirDiags subDir fileDiags '*.data']);
-is3D=length(size(myDiag{1}))==4;
-
-%loop over months and output result
-for tt=1:length(listFiles);
-  filOut=listFiles(tt).name(1:end-5);
-  kk=strfind(filOut,'.00');
-  filOut=[nameDiag filOut(kk(1):end)];
-  if is3D; fldOut=myDiag(:,:,:,tt).*mygrid.mskC;
-  else; fldOut=myDiag(:,:,tt).*mygrid.mskC(:,:,1);
-  end;
-  %interpolate one field
-  tmp1=convert2vector(fldOut);
-  tmp0=1*~isnan(tmp1);
-  tmp1(isnan(tmp1))=0;
-  siz=[size(lon) size(tmp1,2)];
-  tmp0=interp.SPM*tmp0;
-  tmp1=interp.SPM*tmp1;
-  fldOut=reshape(tmp1./tmp0,siz);
-  sizOut=size(fldOut);
-  %create subdirectory
-  if ~isdir([dirOut nameDiag filesep]); mkdir([dirOut nameDiag filesep]); end;
-  %write binary field (masked)
-  write2file([dirOut nameDiag filesep filOut '.data'],fldOut,32,0);
-  %create meta file
-  write2meta([dirOut nameDiag filesep filOut '.data'],sizOut,32,{nameDiag});
-end;
-
-fprintf(['DONE: ' nameDiag ' (in ' num2str(toc) 's)\n']);
+    
+    tic;
+    
+    nameDiag=deblank(listInterp{ii});
+    fprintf(['processing ' nameDiag '... \n']);
+    
+    if ~isempty(dir([dirOut nameDiag filesep nameDiag '*.data']));
+        fprintf(['\n Files were found: ' dirOut nameDiag filesep '*.data']);
+        test0=fprintf('\n Do you want to continue despite risk of overwriting them?');
+        test0=input('\n >> If yes then please type 1 (otherwise just hit return).\n');
+        if isempty(test0)||test0~=1; fprintf(['... skipping ' nameDiag '\n\n']); continue; end;
+    end;
+    
+    if nargin == 6 % passing in pre-loaded fld
+        
+        fldOut = varargin{2};
+        filOut = varargin{3};
+        kk=strfind(filOut,'.00');
+        filOut=[nameDiag filOut(kk(1):end)];
+        tmp1=convert2vector(fldOut);
+        tmp0=1*~isnan(tmp1);
+        tmp1(isnan(tmp1))=0;
+        siz=[size(lon) size(tmp1,2)];
+        tmp0=interp.SPM*tmp0;
+        tmp1=interp.SPM*tmp1;
+        fldOut=reshape(tmp1./tmp0,siz);
+        sizOut=size(fldOut);
+        %create subdirectory
+        if ~isdir([dirOut nameDiag filesep]); mkdir([dirOut nameDiag filesep]); end;
+        %write binary field (masked)
+        write2file([dirOut nameDiag filesep filOut '.data'],fldOut,32,0);
+        %create meta file
+        write2meta([dirOut nameDiag filesep filOut '.data'],sizOut,32,{nameDiag});
+        
+    else
+        
+        jj=find(strcmp(deblank(meta.fldList),nameDiag));
+        myDiag=rdmds2gcmfaces([dirDiags subDir fileDiags '*'],NaN,'rec',jj);
+        listFiles=dir([dirDiags subDir fileDiags '*.data']);
+        is3D=length(size(myDiag{1}))==4;
+        
+        %loop over months and output result
+        for tt=1:length(listFiles);
+            filOut=listFiles(tt).name(1:end-5);
+            kk=strfind(filOut,'.00');
+            filOut=[nameDiag filOut(kk(1):end)];
+            if is3D; fldOut=myDiag(:,:,:,tt).*mygrid.mskC;
+            else; fldOut=myDiag(:,:,tt).*mygrid.mskC(:,:,1);
+            end;
+            %interpolate one field
+            tmp1=convert2vector(fldOut);
+            tmp0=1*~isnan(tmp1);
+            tmp1(isnan(tmp1))=0;
+            siz=[size(lon) size(tmp1,2)];
+            tmp0=interp.SPM*tmp0;
+            tmp1=interp.SPM*tmp1;
+            fldOut=reshape(tmp1./tmp0,siz);
+            sizOut=size(fldOut);
+            %create subdirectory
+            if ~isdir([dirOut nameDiag filesep]); mkdir([dirOut nameDiag filesep]); end;
+            %write binary field (masked)
+            write2file([dirOut nameDiag filesep filOut '.data'],fldOut,32,0);
+            %create meta file
+            write2meta([dirOut nameDiag filesep filOut '.data'],sizOut,32,{nameDiag});
+        end;
+    end
+    
+    fprintf(['DONE: ' nameDiag ' (in ' num2str(toc) 's)\n']);
 end;
 
 %% ======== FUNCTIONS =======
--- gcmfaces_IO/process2nctiles.m
+++ gcmfaces_IO/process2nctiles.m
@@ -1,23 +1,23 @@
 function []=process2nctiles(dirDiags,fileDiags,selectFld,tileSize);
 %process2nctiles(dirDiags,fileDiags);
-% object : convert MITgcm binary output to netcdf files (tiled) 
-% inputs : dirDiags is the directory containing binary model output from 
-%             MITgcm/pkg/diagnostics; dirDiags and its subdirectories will 
+% object : convert MITgcm binary output to netcdf files (tiled)
+% inputs : dirDiags is the directory containing binary model output from
+%             MITgcm/pkg/diagnostics; dirDiags and its subdirectories will
 %             be scanned to locate files that start with the fileDiags prefix;
 %             it should also contain two files called available_diagnostics.log
 %             and README (see below for additional information).
 %         fileDiags the file name base e.g. 'state_2d_set1'
-%           By default : all variables in e.g. 'state_2d_set1*' 
+%           By default : all variables in e.g. 'state_2d_set1*'
 %           files will be processed, and written individually to
 %           nctiles (tiled netcdf) that will be located in 'nctiles/'
-%         selectFld (optional) can be specified as, e.g., 1, or 'ETAN', or {'ETAN'}; 
-%            {'ETAN'};; if selectFld is left un-specified or specified as empty 
-%            then all fields listed in [fileDiags '*.meta'] will be processed; 
+%         selectFld (optional) can be specified as, e.g., 1, or 'ETAN', or {'ETAN'};
+%            {'ETAN'};; if selectFld is left un-specified or specified as empty
+%            then all fields listed in [fileDiags '*.meta'] will be processed;
 %            if selectFld is a vector of positive integers then these will be treated
 %            as indices in the fields list; if selectFld is a character string or a
 %            cell array of strings then each string will be treated as the field name.
 %         tileSize (optional) can be specified (e.g., [90 90]); if otherwise
-%            then tile sizes will be set to face sizes (i.e., mygrid.facesSize). 
+%            then tile sizes will be set to face sizes (i.e., mygrid.facesSize).
 % Output : (netcdf files)
 %
 % Notes: available_diagnostics.log (need for documentation ...)
@@ -32,7 +32,8 @@ if isempty(whos('selectFld')); selectFld=''; end;
 if isempty(whos('tileSize')); tileSize=[]; end;
 
 %replace time series with monthly climatology?
-doClim=1; if doClim; fprintf('\n Creating monthly climatology (doClim=1) \n\n'); end;
+%doClim=1; 
+doClim=0; if doClim; fprintf('\n Creating monthly climatology (doClim=1) \n\n'); end;
 
 %needed files
 filAvailDiag=[dirDiags 'available_diagnostics.log'];
@@ -52,168 +53,168 @@ if ~isdir(dirOut); mkdir(dirOut); end;
 [subDir]=rdmds_search_subdirs(dirDiags,fileDiags);
 
 if isempty(subDir);
-  error(['file ' fileDiags ' was not found']);
+    error(['file ' fileDiags ' was not found']);
 else;
-  dirIn=[dirDiags subDir];
-  nn=length(dir([dirIn fileDiags '*data']));
-  fprintf('%s (%d files) was found in %s \n',fileDiags,nn,dirIn);
+    dirIn=[dirDiags subDir];
+    nn=length(dir([dirIn fileDiags '*data']));
+    fprintf('%s (%d files) was found in %s \n',fileDiags,nn,dirIn);
 end;
 
 %set list of variables to process
 if ~isempty(selectFld)&&ischar(selectFld);
-   listFlds={selectFld};
+    listFlds={selectFld};
 elseif ~isempty(selectFld)&&iscell(selectFld);
-   listFlds=selectFld;
+    listFlds=selectFld;
 else;
-   meta=rdmds_meta([dirIn fileDiags]);
-   listFlds=meta.fldList;
-   if isnumeric(selectFld);
-     listFlds={listFlds{selectFld}};
-   end;
+    meta=rdmds_meta([dirIn fileDiags]);
+    listFlds=meta.fldList;
+    if isnumeric(selectFld);
+        listFlds={listFlds{selectFld}};
+    end;
 end;
 
 %determine map of tile indices (by default tiles=faces)
 if isempty(tileSize);
-  tileNo=mygrid.XC; 
-  for ff=1:mygrid.nFaces; tileNo{ff}(:)=ff; end;
+    tileNo=mygrid.XC;
+    for ff=1:mygrid.nFaces; tileNo{ff}(:)=ff; end;
 else;
-  tileNo=gcmfaces_loc_tile(tileSize(1),tileSize(2));
+    tileNo=gcmfaces_loc_tile(tileSize(1),tileSize(2));
 end;
 
 %now do the actual processing
 for vv=1:length(listFlds);
-nameDiag=deblank(listFlds{vv});
-fprintf(['processing ' nameDiag '... \n']);
-
-%get meta information
-meta=rdmds_meta([dirIn fileDiags]);
-irec=find(strcmp(deblank(meta.fldList),nameDiag));
-if length(irec)~=1; error('field not in file\n'); end;
-
-%read time series
-myDiag=rdmds2gcmfaces([dirIn fileDiags '*'],NaN,'rec',irec);
-
-%set ancilliary time variable
-nn=length(size(myDiag{1}));
-nn=size(myDiag{1},nn);
-tim=[1992*ones(nn,1) [1:nn]' 15*ones(nn,1)];
-tim=datenum(tim)-datenum([1992 1 1]);
-begtim=[1992*ones(nn,1) [1:nn]' ones(nn,1)];
-begtim=datenum(begtim)-datenum([1992 1 1]);
-endtim=[1992*ones(nn,1) [1:nn]' 1+ones(nn,1)];
-endtim=datenum(endtim)-datenum([1992 1 1]);
-timUnits='days since 1992-1-1 0:0:0';
-clmbnds=[];
-
-%if doClim then replace time series with monthly climatology and assign climatology_bounds variable
-if doClim; 
- myDiag=compClim(myDiag);
- %set tim to first year values for case of unsupported 'climatology' attribute (see below)  
- tim=tim(1:12);
- %'climatology' attribute + 'climatology_bounds' variable will be added as shown at
- %http://cfconventions.org/cf-conventions/v1.6.0/cf-conventions.html#climatological-statistics
- for tt=1:12; 
-   tmpb=begtim(tt:12:nn); tmpe=endtim(tt:12:nn) ;
-   clmbnds=[clmbnds;[tmpb(1) tmpe(end)]];
- end;
-end;
-
-%get units and long name from available_diagnostics.log
-[avail_diag]=read_avail_diag(filAvailDiag,nameDiag);
-
-%rename variable if needed
-nameDiagOut=nameDiag;
-if ~isempty(filRename);
-  load(filRename); ii=find(strcmp(listNameIn,nameDiag));
-  if ~isempty(ii); nameDiagOut=listNameOut{ii}; end;
-end;
-
-%get description of estimate from README
-[rdm]=read_readme(filReadme);
-disp(rdm');
-
-%set output directory/file name
-myFile=[dirOut nameDiagOut];%first instance is for subdirectory name
-if ~isdir(myFile); mkdir(myFile); end;
-myFile=[myFile filesep nameDiagOut];%second instance is for file name base
-
-%get grid params
-[grid_diag]=set_grid_diag(avail_diag);
-
-%apply mask(, and convert to land mask)
-if isfield(grid_diag,'msk');
-  msk=grid_diag.msk;
-  if length(size(myDiag{1}))==3;
-    msk=repmat(msk(:,:,1),[1 1 size(myDiag{1},3)]);
-  else;
-    msk=repmat(msk,[1 1 1 size(myDiag{1},4)]);
-  end;
-  myDiag=myDiag.*msk;
-  clear msk;
-  %
-  %land=isnan(grid_diag.msk);
-end;
-
-%set 'coord' attribute
-if avail_diag.nr~=1;
-  coord='lon lat dep tim';
-  dimlist={'t',grid_diag.dimlist{:}};
-  dimname={'Time coordinate','Cartesian coordinate 3','Cartesian coordinate 2','Cartesian coordinate 1'};
-else;
-  coord='lon lat tim';
-  dimlist={'t',grid_diag.dimlist{:}};
-  dimname={'Time coordinate','Cartesian coordinate 2','Cartesian coordinate 1'};
-end;
-
-%create netcdf file using write2nctiles
-doCreate=1; myDiag=single(myDiag); 
-dimlist=write2nctiles(myFile,myDiag,doCreate,{'tileNo',tileNo},...
-    {'fldName',nameDiagOut},{'longName',avail_diag.longNameDiag},{'xtype','float'},...
-    {'units',avail_diag.units},{'descr',nameDiagOut},{'coord',coord},{'dimlist',dimlist},...
-    {'dimname',dimname},{'clmbnds',clmbnds},{'rdm',rdm});
-
-%determine relevant dimensions
-for ff=1:length(dimlist);
-  dim.tim{ff}={dimlist{ff}{1}};
-  dim.twoD{ff}={dimlist{ff}{end-1:end}};
-  if avail_diag.nr~=1;
-    dim.threeD{ff}={dimlist{ff}{end-2:end}};
-    dim.dep{ff}={dimlist{ff}{end-2}};
-  else;
-    dim.threeD{ff}=dim.twoD{ff};
-    dim.dep{ff}=[];
-  end;
-end;
-
-%prepare to add fields
-doCreate=0;
-
-%now add fields
-write2nctiles(myFile,grid_diag.lon,doCreate,{'tileNo',tileNo},...
-  {'fldName','lon'},{'units','degrees_east'},{'dimIn',dim.twoD});
-write2nctiles(myFile,grid_diag.lat,doCreate,{'tileNo',tileNo},...
-  {'fldName','lat'},{'units','degrees_north'},{'dimIn',dim.twoD});
-write2nctiles(myFile,tim,doCreate,{'tileNo',tileNo},{'fldName','tim'},...
-  {'longName','time'},{'units',timUnits},{'dimIn',dim.tim},{'clmbnds',clmbnds});
-if isfield(grid_diag,'dep');
-  write2nctiles(myFile,grid_diag.dep,doCreate,{'tileNo',tileNo},...
-    {'fldName','dep'},{'units','m'},{'dimIn',dim.dep});
-  if isfield(grid_diag,'dz');
-    write2nctiles(myFile,grid_diag.dz,doCreate,{'tileNo',tileNo},...
-      {'fldName','thic'},{'units','m'},{'dimIn',dim.dep});
-  end;
-end;
-if isfield(grid_diag,'msk');
-  write2nctiles(myFile,grid_diag.msk,doCreate,{'tileNo',tileNo},...
-    {'fldName','land'},{'units','1'},{'longName','land mask'},{'dimIn',dim.threeD});
-end;
-if isfield(grid_diag,'area');
-  write2nctiles(myFile,grid_diag.area,doCreate,{'tileNo',tileNo},...
-    {'fldName','area'},{'units','m^2'},{'longName','grid cell area'},{'dimIn',dim.twoD});
-end;
-
-clear myDiag;
-
+    nameDiag=deblank(listFlds{vv});
+    fprintf(['processing ' nameDiag '... \n']);
+    
+    %get meta information
+    meta=rdmds_meta([dirIn fileDiags]);
+    irec=find(strcmp(deblank(meta.fldList),nameDiag));
+    if length(irec)~=1; error('field not in file\n'); end;
+    
+    %read time series
+    myDiag=rdmds2gcmfaces([dirIn fileDiags '*'],NaN,'rec',irec);
+    
+    %set ancilliary time variable
+    nn=length(size(myDiag{1}));
+    nn=size(myDiag{1},nn);
+    tim=[1992*ones(nn,1) [1:nn]' 15*ones(nn,1)];
+    tim=datenum(tim)-datenum([1992 1 1]);
+    begtim=[1992*ones(nn,1) [1:nn]' ones(nn,1)];
+    begtim=datenum(begtim)-datenum([1992 1 1]);
+    endtim=[1992*ones(nn,1) [1:nn]' 1+ones(nn,1)];
+    endtim=datenum(endtim)-datenum([1992 1 1]);
+    timUnits='days since 1992-1-1 0:0:0';
+    clmbnds=[];
+    
+    %if doClim then replace time series with monthly climatology and assign climatology_bounds variable
+    if doClim;
+        myDiag=compClim(myDiag);
+        %set tim to first year values for case of unsupported 'climatology' attribute (see below)
+        tim=tim(1:12);
+        %'climatology' attribute + 'climatology_bounds' variable will be added as shown at
+        %http://cfconventions.org/cf-conventions/v1.6.0/cf-conventions.html#climatological-statistics
+        for tt=1:12;
+            tmpb=begtim(tt:12:nn); tmpe=endtim(tt:12:nn) ;
+            clmbnds=[clmbnds;[tmpb(1) tmpe(end)]];
+        end;
+    end;
+   
+   %get units and long name from available_diagnostics.log
+   [avail_diag]=read_avail_diag(filAvailDiag,nameDiag);
+     
+    %rename variable if needed
+    nameDiagOut=nameDiag;
+    if ~isempty(filRename);
+        load(filRename); ii=find(strcmp(listNameIn,nameDiag));
+        if ~isempty(ii); nameDiagOut=listNameOut{ii}; end;
+    end;
+    
+    %get description of estimate from README
+    [rdm]=read_readme(filReadme);
+    disp(rdm');
+    
+    %set output directory/file name
+    myFile=[dirOut nameDiagOut];%first instance is for subdirectory name
+    if ~isdir(myFile); mkdir(myFile); end;
+    myFile=[myFile filesep nameDiagOut];%second instance is for file name base
+    
+    %get grid params
+    [grid_diag]=set_grid_diag(avail_diag);
+    
+    %apply mask(, and convert to land mask)
+    if isfield(grid_diag,'msk');
+        msk=grid_diag.msk;
+        if length(size(myDiag{1}))==3;
+            msk=repmat(msk(:,:,1),[1 1 size(myDiag{1},3)]);
+        else;
+            msk=repmat(msk,[1 1 1 size(myDiag{1},4)]);
+        end;
+        myDiag=myDiag.*msk;
+        clear msk;
+        %
+        %land=isnan(grid_diag.msk);
+    end;
+    
+    %set 'coord' attribute
+    if avail_diag.nr~=1;
+        coord='lon lat dep tim';
+        dimlist={'t',grid_diag.dimlist{:}};
+        dimname={'Time coordinate','Cartesian coordinate 3','Cartesian coordinate 2','Cartesian coordinate 1'};
+    else;
+        coord='lon lat tim';
+        dimlist={'t',grid_diag.dimlist{:}};
+        dimname={'Time coordinate','Cartesian coordinate 2','Cartesian coordinate 1'};
+    end;
+    
+    %create netcdf file using write2nctiles
+    doCreate=1; myDiag=single(myDiag);
+    dimlist=write2nctiles(myFile,myDiag,doCreate,{'tileNo',tileNo},...
+        {'fldName',nameDiagOut},{'longName',avail_diag.longNameDiag},{'xtype','float'},...
+        {'units',avail_diag.units},{'descr',nameDiagOut},{'coord',coord},{'dimlist',dimlist},...
+        {'dimname',dimname},{'clmbnds',clmbnds},{'rdm',rdm});
+    
+    %determine relevant dimensions
+    for ff=1:length(dimlist);
+        dim.tim{ff}={dimlist{ff}{1}};
+        dim.twoD{ff}={dimlist{ff}{end-1:end}};
+        if avail_diag.nr~=1;
+            dim.threeD{ff}={dimlist{ff}{end-2:end}};
+            dim.dep{ff}={dimlist{ff}{end-2}};
+        else;
+            dim.threeD{ff}=dim.twoD{ff};
+            dim.dep{ff}=[];
+        end;
+    end;
+    
+    %prepare to add fields
+    doCreate=0;
+    
+    %now add fields
+    write2nctiles(myFile,grid_diag.lon,doCreate,{'tileNo',tileNo},...
+        {'fldName','lon'},{'units','degrees_east'},{'dimIn',dim.twoD});
+    write2nctiles(myFile,grid_diag.lat,doCreate,{'tileNo',tileNo},...
+        {'fldName','lat'},{'units','degrees_north'},{'dimIn',dim.twoD});
+    write2nctiles(myFile,tim,doCreate,{'tileNo',tileNo},{'fldName','tim'},...
+        {'longName','time'},{'units',timUnits},{'dimIn',dim.tim},{'clmbnds',clmbnds});
+    if isfield(grid_diag,'dep');
+        write2nctiles(myFile,grid_diag.dep,doCreate,{'tileNo',tileNo},...
+            {'fldName','dep'},{'units','m'},{'dimIn',dim.dep});
+        if isfield(grid_diag,'dz');
+            write2nctiles(myFile,grid_diag.dz,doCreate,{'tileNo',tileNo},...
+                {'fldName','thic'},{'units','m'},{'dimIn',dim.dep});
+        end;
+    end;
+    if isfield(grid_diag,'msk');
+        write2nctiles(myFile,grid_diag.msk,doCreate,{'tileNo',tileNo},...
+            {'fldName','land'},{'units','1'},{'longName','land mask'},{'dimIn',dim.threeD});
+    end;
+    if isfield(grid_diag,'area');
+        write2nctiles(myFile,grid_diag.area,doCreate,{'tileNo',tileNo},...
+            {'fldName','area'},{'units','m^2'},{'longName','grid cell area'},{'dimIn',dim.twoD});
+    end;
+    
+    clear myDiag;
+    
 end;%for vv=1:length(listFlds);
 
 %%
@@ -329,7 +330,7 @@ if ndim==3; FLD=NaN*fld(:,:,1:12); end;
 if ndim==4; FLD=NaN*fld(:,:,:,1:12); end;
 
 for mm=1:12;
-if ndim==3; FLD(:,:,mm)=mean(fld(:,:,mm:12:12*nyear),ndim); end;
-if ndim==4; FLD(:,:,:,mm)=mean(fld(:,:,:,mm:12:12*nyear),ndim); end;
+    if ndim==3; FLD(:,:,mm)=mean(fld(:,:,mm:12:12*nyear),ndim); end;
+    if ndim==4; FLD(:,:,:,mm)=mean(fld(:,:,:,mm:12:12*nyear),ndim); end;
 end;
 
